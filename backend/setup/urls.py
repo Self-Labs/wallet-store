030510f04f6438f0820054cd610401917e6a489b
Gustavo Cateim <cateim@gmail.com>
2025-12-21 15:07:47 -0300

    Fix: Rename ProductViewSet to ProdutoViewSet in urls.py

--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import ProductViewSet
+from core.views import ProdutoViewSet
 from sales.views import OrderViewSet
 
 # Centralizando as rotas de todos os apps aqui
 router = DefaultRouter()
-router.register(r'produtos', ProductViewSet)
+router.register(r'produtos', ProdutoViewSet)
 router.register(r'pedidos', OrderViewSet)
 
 urlpatterns = [
